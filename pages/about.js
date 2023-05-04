d32db0c1c1bd0abd4e415c5b181c6cac968e11a6
Liam2263 <65407720+Liam2263@users.noreply.github.com>
2023-05-04 13:21:24 -0400

    Delete pages directory

--- pages/about.js
+++
@@ -1,8 +0,0 @@
-export default function About(props) {
-  return (
-    <>
-      <h2>About</h2>
-      <p>My Rocket League bots are trained using Proximal Policy Optimization (PPO), which is facilitated through stable-baselines. This framework is incredibly helpful in obtaining PPO for my bots. Additionally, I utilize an environment called RLGym to train my Rocket League bots, as it proves to be a useful tool for this purpose.</p>
-    </>
-  )
-}
