64a350aee0ae0daa93cb86eeb37d86ea6c04d9b4
Liam2263 <65407720+Liam2263@users.noreply.github.com>
2023-05-06 09:23:43 -0400

    Delete pages

--- pages/about.js
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <>
-      <h2 className="text-4xl pt-6 font-semibold">About</h2>
-      <p>My Rocket League bots are trained using Proximal Policy Optimization (PPO), which is facilitated through stable-baselines. This framework is incredibly helpful in obtaining PPO for my bots. Additionally, I utilize an environment called RLGym to train my Rocket League bots, as it proves to be a useful tool for this purpose.</p>
-    </>
-  )
-}
